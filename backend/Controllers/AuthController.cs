fef1aee95edeb82c30c64299a453ab27641de2ed
Luiz Felipe <raziel_libertino@hotmail.com>
2020-11-20 23:29:53 -0400

    auth controller

---
+++ backend/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+using System.Threading.Tasks;
+using backend.Repositories;
+using backend.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace backend.Controllers
+{
+  [ApiController]
+  [Route("api/v1/[controller]")]
+  [AllowAnonymous]
+  public class AuthController : ControllerBase
+  {
+    private readonly IAuthRepository repo;
+    private readonly IConfiguration config;
+    public AuthController(IAuthRepository repo, IConfiguration config)
+    {
+      this.config = config;
+      this.repo = repo;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(string email, string password)
+    {
+        var user = await repo.Login(email, password);
+
+        if (user == null) return BadRequest(new { message = "Email ou senha inválida"});
+
+        var token = TokenService.GenerateTokenUser(user, config);
+
+        return Ok(new { token, user});
+    }
+  }
+}
